b579f04df4dabfbe14f4cfd4b8030d4af7f73f1d
Peter Vanhee <peter.vanhee@gmail.com>
2015-12-27 11:00:07 +0100

    Make the preview method accept any kind of ContentEntityInterface

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,6 +8,7 @@
 namespace Drupal\view_modes_display\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\node\NodeInterface;
 
 /**
@@ -22,18 +23,40 @@ class DefaultController extends ControllerBase {
    *
    * @return string
    */
-  public function preview(NodeInterface $node) {
+  public function preview(ContentEntityInterface $node) {
     $entity_manager = \Drupal::entityManager();
     $view_modes_info = $entity_manager->getViewModes('node');
 
-    // Loop through the view modes
+    $config_prefix = 'core.entity_view_display';
+    $entity_type_id = $node->getEntityType()->id();
+
+    $ids = \Drupal::configFactory()->listAll($config_prefix . '.' . $entity_type_id . '.' . $node->bundle() . '.');
+
+    $load_ids = array();
+    foreach ($ids as $id) {
+      $config_id = str_replace($config_prefix . '.', '', $id);
+      list(,, $display_mode) = explode('.', $config_id);
+      $load_ids[] = $config_id;
+    }
+
+    $enabled_display_modes = array();
+    $displays = $entity_manager->getStorage('entity_view_display')->loadMultiple($load_ids);
+    foreach ($displays as $display) {
+      if ($display->status()) {
+        $enabled_display_modes[] = $display->get('mode');
+      }
+    }
+
+    // Loop through the view modes and render in-place
     $build = array();
     foreach ($view_modes_info as $view_mode_name => $view_mode_info) {
-      $build[] = [
-        '#prefix' => '<div class="view-mode-list-item view-mode-list-item-' . $view_mode_name . '"><h1>' . $view_mode_info['label'] . '</h1>',
-        '#markup' => render(entity_view($node, $view_mode_name)),
-        '#suffix' => '</div>',
-      ];
+      if (in_array($view_mode_name, $enabled_display_modes)) {
+        $build[] = [
+          '#prefix' => '<div class="view-mode-list-item view-mode-list-item-' . $view_mode_name . '"><h1>' . $view_mode_info['label'] . '</h1>',
+          '#markup' => render(entity_view($node, $view_mode_name)),
+          '#suffix' => '</div>',
+        ];
+      }
     }
 
     return $build;
